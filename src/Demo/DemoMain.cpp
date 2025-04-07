5d0da977d089faec5c408d91822d5fbe20fcb244
James Xue <33576713+James-Xue@users.noreply.github.com>
2025-04-07 22:36:05 +0800

    1. Add a first CMakeLists.txt for the whole project. 2. Update the Demo/DemoMain.cpp and Demo/DemoMain.h.

--- src/Demo/DemoMain.cpp
+++ src/Demo/DemoMain.cpp
@@ -7,18 +7,18 @@ void CleanupApplication();
 
 int main(int argc, char* argv[])
 {
-    // Initialize the application
-    if (false == InitApplication(argc, argv))
-    {
-        std::cerr << "Failed to initialize application."
-                  << std::endl;
-        return -1;
-    }
+    //// Initialize the application
+    //if (false == InitApplication(argc, argv))
+    //{
+    //    std::cerr << "Failed to initialize application."
+    //              << std::endl;
+    //    return -1;
+    //}
 
-    // Run the main application loop
-    RunApplicationLoop();
+    //// Run the main application loop
+    //RunApplicationLoop();
 
-    // Clean up and exit
-    CleanupApplication();
+    //// Clean up and exit
+    //CleanupApplication();
     return 0;
 }
