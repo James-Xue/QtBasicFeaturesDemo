f6a4bea5386a2b9ee4c64329d32afa357980bdbd
James Xue <33576713+James-Xue@users.noreply.github.com>
2025-04-14 21:47:03 +0800

    1. Add more .cpp files and .h files about Qt module.

---
+++ src/Qt/QtCore/qtcore_tracepoints_p.h
@@ -0,0 +1,1 @@
+#include <QtCore/qtrace_p.h>
--- src/Qt/QtCore/qtest.cpp
+++ src/Qt/QtCore/qtest.cpp
@@ -3,13 +3,5 @@
 
 int main(const int/* argc*/, char */*argv*/[])
 {
-    //Q_UNUSED(argc);
-    //Q_UNUSED(argv);
-
-    // Check if the configuration strings are correctly defined
-    //if (qt_configure_strs[0] != "qt_configure_strs") {
-    //    return 1; // Error: Configuration strings mismatch
-    //}
-
-    return 0; // Success
+    return 0;
 }
