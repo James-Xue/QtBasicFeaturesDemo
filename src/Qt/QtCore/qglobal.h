f88236410eea43026280f47f88b672b1f65a45c2
James Xue <33576713+James-Xue@users.noreply.github.com>
2025-04-19 16:05:13 +0800

    1. Add more .cpp and .h files for Qt module.

--- src/Qt/QtCore/qglobal.h
+++ src/Qt/QtCore/qglobal.h
@@ -25,51 +25,47 @@
     #include <QtCore/qtversionchecks.h>
     #include <QtCore/qtconfigmacros.h>
     #include <QtCore/qtcoreexports.h>
-
     #include <QtCore/qtpreprocessorsupport.h>
-
     #include <QtCore/qsystemdetection.h>
     #include <QtCore/qprocessordetection.h>
     #include <QtCore/qcompilerdetection.h>
 
     #ifndef __ASSEMBLER__
-    #  include <QtCore/qassert.h>
-    #  include <QtCore/qtnoop.h>
-    #  include <QtCore/qtypes.h>
+        #include <QtCore/qassert.h>
+        #include <QtCore/qtnoop.h>
+        #include <QtCore/qtypes.h>
     #endif /* !__ASSEMBLER__ */
+
     #include <QtCore/qtversion.h>
 
     #if defined(__cplusplus)
+        // We need to keep QTypeInfo, QSysInfo, QFlags, qDebug & family in qglobal.h for compatibility with Qt 4.
+        // Be careful when changing the order of these files.
+        #include <QtCore/qtclasshelpermacros.h>
+        #include <QtCore/qtypeinfo.h>
+        #include <QtCore/qsysinfo.h>
+        #include <QtCore/qlogging.h>
+        #include <QtCore/qflags.h>
+        #include <QtCore/qatomic.h>
+        #include <QtCore/qconstructormacros.h>
+        #include <QtCore/qdarwinhelpers.h>
+        #include <QtCore/qexceptionhandling.h>
+        #include <QtCore/qforeach.h>
+        #include <QtCore/qfunctionpointer.h>
+        #include <QtCore/qglobalstatic.h>
+        #include <QtCore/qmalloc.h>
+        #include <QtCore/qminmax.h>
+        #include <QtCore/qnumeric.h>
+        #include <QtCore/qoverload.h>
+        #include <QtCore/qswap.h>
+        #include <QtCore/qtdeprecationmarkers.h>
+        #include <QtCore/qtenvironmentvariables.h>
+        #include <QtCore/qtresource.h>
+        #include <QtCore/qttranslation.h>
+        #include <QtCore/qttypetraits.h>
 
-    #include <QtCore/qtclasshelpermacros.h>
-
-    // We need to keep QTypeInfo, QSysInfo, QFlags, qDebug & family in qglobal.h for compatibility with Qt 4.
-    // Be careful when changing the order of these files.
-    #include <QtCore/qtypeinfo.h>
-    #include <QtCore/qsysinfo.h>
-    #include <QtCore/qlogging.h>
-
-    #include <QtCore/qflags.h>
-
-    #include <QtCore/qatomic.h>
-    #include <QtCore/qconstructormacros.h>
-    #include <QtCore/qdarwinhelpers.h>
-    #include <QtCore/qexceptionhandling.h>
-    #include <QtCore/qforeach.h>
-    #include <QtCore/qfunctionpointer.h>
-    #include <QtCore/qglobalstatic.h>
-    #include <QtCore/qmalloc.h>
-    #include <QtCore/qminmax.h>
-    #include <QtCore/qnumeric.h>
-    #include <QtCore/qoverload.h>
-    #include <QtCore/qswap.h>
-    #include <QtCore/qtdeprecationmarkers.h>
-    #include <QtCore/qtenvironmentvariables.h>
-    #include <QtCore/qtresource.h>
-    #include <QtCore/qttranslation.h>
-    #include <QtCore/qttypetraits.h>
-    #if QT_CONFIG(version_tagging)
-    #include <QtCore/qversiontagging.h>
-    #endif
+        #if QT_CONFIG(version_tagging)
+            #include <QtCore/qversiontagging.h>
+        #endif
     #endif /* __cplusplus */
 #endif /* QGLOBAL_H */
