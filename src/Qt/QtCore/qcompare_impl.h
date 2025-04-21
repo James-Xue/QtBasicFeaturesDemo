6931e606f9048eec5f1faeed0d88963acf093288
James Xue <33576713+James-Xue@users.noreply.github.com>
2025-04-21 17:54:13 +0800

    1. Add more .cpp and .h files for Qt module.

--- src/Qt/QtCore/qcompare_impl.h
+++ src/Qt/QtCore/qcompare_impl.h
@@ -2,33 +2,33 @@
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #ifndef QCOMPARE_IMPL_H
-#define QCOMPARE_IMPL_H
+    #define QCOMPARE_IMPL_H
 
-#if 0
-#pragma qt_sync_skip_header_check
-#pragma qt_sync_stop_processing
-#endif
+    #if 0
+        #pragma qt_sync_skip_header_check
+        #pragma qt_sync_stop_processing
+    #endif
 
-#include <QtCore/qtconfigmacros.h>
-#include <QtCore/qcompilerdetection.h>
+    #include <QtCore/qtconfigmacros.h>
+    #include <QtCore/qcompilerdetection.h>
 
-QT_BEGIN_NAMESPACE
+    QT_BEGIN_NAMESPACE
 
-namespace QtPrivate {
+    namespace QtPrivate
+    {
+        // [cmp.categories.pre] / 3, but using a safe bool trick
+        // and also rejecting std::nullptr_t (unlike the example)
+        class CompareAgainstLiteralZero
+        {
+        public:
+            using SafeZero = void (CompareAgainstLiteralZero::*)();
 
-// [cmp.categories.pre] / 3, but using a safe bool trick
-// and also rejecting std::nullptr_t (unlike the example)
-class CompareAgainstLiteralZero {
-public:
-    using SafeZero = void (CompareAgainstLiteralZero::*)();
-    Q_IMPLICIT constexpr CompareAgainstLiteralZero(SafeZero) noexcept {}
+            Q_IMPLICIT constexpr CompareAgainstLiteralZero(SafeZero) noexcept {}
 
-    template <typename T, std::enable_if_t<std::is_null_pointer_v<T>, bool> = true>
-    CompareAgainstLiteralZero(T) = delete;
-};
+            template <typename T, std::enable_if_t<std::is_null_pointer_v<T>, bool> = true>
+            CompareAgainstLiteralZero(T) = delete;
+        };
+    } // namespace QtPrivate
 
-} // namespace QtPrivate
-
-QT_END_NAMESPACE
-
+    QT_END_NAMESPACE
 #endif // QCOMPARE_IMPL_H
