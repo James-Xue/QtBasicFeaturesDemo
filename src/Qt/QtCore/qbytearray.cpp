157afe03ed95c363bd57ded6e627e2d97fc82d83
James Xue <33576713+James-Xue@users.noreply.github.com>
2025-04-27 12:14:05 +0800

    1. Completed the compilation of qmetatype.cpp.

--- src/Qt/QtCore/qbytearray.cpp
+++ src/Qt/QtCore/qbytearray.cpp
@@ -2,6 +2,7 @@
 // Copyright (C) 2016 Intel Corporation.
 // Copyright (C) 2019 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com, author Giuseppe D'Angelo <giuseppe.dangelo@kdab.com>
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+#define  _CRT_SECURE_NO_WARNINGS
 
 #include "qbytearray.h"
 #include "qbytearraymatcher.h"
