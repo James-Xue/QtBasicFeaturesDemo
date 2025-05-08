2948c88c4ca01a1ca51dd86016c87778779d4d3f
James Xue <33576713+James-Xue@users.noreply.github.com>
2025-05-08 23:29:01 +0800

    1. 调整cmakelists的内容,使其支持自动拷贝项目中的可执行文件到bin目录下;

--- src/BioSenseMain/BioSenseGuiMain.cpp
+++ src/BioSenseMain/BioSenseGuiMain.cpp
@@ -1,21 +1,54 @@
 // STL
 //#include <iostream>
 
-// Qt
 //#include <QtCore/qchar.h>
 
 // Self
 #include "BioSenseGuiMain.h"
 #include "CSVHelper.h"
 
+// Qt
+#include <QtWidgets/QApplication>
 
-int main(const int /*argc*/, char */*argv*/[])
+int main(int argc, char *argv[])
 {
     // 1. read CSV file
-    Demo::CSVHelper csvHelper;
-    csvHelper.ReadCSV(
-        u8"C:/Users/52738/Documents/9_Tmp/20250116/20250116rcd84d7677220250116095027_p1.csv");
+    //Demo::CSVHelper csvHelper;
+    //csvHelper.ReadCSV(
+    //    u8"C:/Users/52738/Documents/9_Tmp/20250116/20250116rcd84d7677220250116095027_p1.csv");
 
-    // 2. return 0
-    return 0;
+    QApplication app(argc, argv);
+
+    //// 创建一个折线系列
+    //QLineSeries *series = new QLineSeries();
+
+    //// 添加数据点
+    //series->append(0, 6);
+    //series->append(2, 4);
+    //series->append(3, 8);
+    //series->append(7, 4);
+    //series->append(10, 5);
+
+    //// 创建一个图表并添加系列
+    //QChart *chart = new QChart();
+    //chart->addSeries(series);
+
+    //// 设置图表标题
+    //chart->setTitle("Simple line chart example");
+
+    //// 创建坐标轴
+    //chart->createDefaultAxes();
+
+    //// 创建一个图表视图并设置图表
+    //QChartView *chartView = new QChartView(chart);
+    //chartView->setRenderHint(QPainter::Antialiasing);
+
+    //// 创建一个窗口并设置图表视图
+    //QMainWindow window;
+    //window.setCentralWidget(chartView);
+    //window.resize(800, 600);
+    //window.show();
+
+    // 进入 Qt 的事件循环中
+    return app.exec();
 }
