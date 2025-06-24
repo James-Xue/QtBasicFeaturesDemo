826a7af8ae01a14822a0dc1f07f9990917836b91
James Xue <33576713+James-Xue@users.noreply.github.com>
2025-06-24 22:14:12 +0800

    1. 调整代码逻辑,简化代码结构;

--- src/BioSenseMain/BioSenseGuiMain.cpp
+++ src/BioSenseMain/BioSenseGuiMain.cpp
@@ -14,26 +14,26 @@
 
 int main(int argc, char *argv[])
 {
-    // 1. Initialize app
-    QApplication app(argc, argv);
-
-    // 2. Create Qml engine and load qml
-    QQuickStyle::setStyle("Fusion");  // or "Material"、"Basic"
-    QQmlApplicationEngine engine;
-
-    // 3. Registered Property
-    Bio::DataInputModule dataInput;
-    engine.rootContext()->setContextProperty("dataInput", &dataInput);
-
-    // 4. Check if the file exists
+    // 1. Check if the Main.qml exists
     const QString sPath = QString::fromUtf8(u8":/UI/Main.qml");
-    QFile file(sPath);
-    if (false == file.exists())
+    if (false == QFile::exists(sPath))
     {
         qDebug() << "Error: QML file not found in resources!";
         qDebug() << "Current resources:";
+        Q_ASSERT(false);
         return -1;
     }
+
+    // 2. Initialize app
+    QApplication app(argc, argv);
+
+    // 3. Create Qml engine and load qml, set Style "Fusion" or "Material"、"Basic"
+    //QQuickStyle::setStyle("Fusion");
+    QQmlApplicationEngine engine;
+
+    // 4. Registered Property
+    Bio::DataInputModule dataInput;
+    engine.rootContext()->setContextProperty("dataInput", &dataInput);
 
     // 5. Load QML file
     engine.load(sPath);
