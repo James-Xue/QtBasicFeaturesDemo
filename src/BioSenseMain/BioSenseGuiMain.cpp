eea5731053615e1b6ed3b443139539e0a204f4cb
James Xue <33576713+James-Xue@users.noreply.github.com>
2025-06-28 21:40:31 +0800

    1. 抓取运行时输出到命令行的错误信息和警告信息,并输出到指定的日志文件中; 2. 增加git忽略文件.

--- src/BioSenseMain/BioSenseGuiMain.cpp
+++ src/BioSenseMain/BioSenseGuiMain.cpp
@@ -1,5 +1,9 @@
 // Qt
 #include <QFile>
+#include <QTextStream>
+#include <QDateTime>
+#include <QMutex>
+#include <QDebug>
 #include <QApplication>
 #include <QIcon>
 #include <QQmlApplicationEngine>
@@ -9,8 +13,39 @@
 #include "BioSenseGuiMain.h"
 #include "DataInputModule.h"
 
+QFile g_logFile;
+QMutex g_logMutex;
+
+void myMessageHandler(QtMsgType type, const QMessageLogContext &/*context*/, const QString &msg)
+{
+    QMutexLocker locker(&g_logMutex);
+    if (!g_logFile.isOpen())
+        return;
+
+    QTextStream out(&g_logFile);
+    QString time = QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss.zzz");
+    QString typeStr;
+    switch (type)
+    {
+    case QtDebugMsg:    typeStr = "DEBUG"; break;
+    case QtInfoMsg:     typeStr = "INFO"; break;
+    case QtWarningMsg:  typeStr = "WARNING"; break;
+    case QtCriticalMsg: typeStr = "CRITICAL"; break;
+    case QtFatalMsg:    typeStr = "FATAL"; break;
+    }
+    out << QString("[%1] [%2] %3\n").arg(time, typeStr, msg);
+    out.flush();
+    if (type == QtFatalMsg)
+        abort();
+}
+
 int main(int argc, char *argv[])
 {
+    // 日志重定向
+    g_logFile.setFileName("../../02_BioSense/test/BiosenseLog.txt");
+    g_logFile.open(QIODevice::Append | QIODevice::Text);
+    qInstallMessageHandler(myMessageHandler);
+
     // 1. Check if the Main.qml exists
     const QString sPath = QString::fromUtf8(u8":/QML/Main.qml");
     if (false == QFile::exists(sPath))
